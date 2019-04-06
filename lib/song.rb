af85cb3140f01f6c2c6f816daa581e0e146c82e7
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-06 13:12:52 +0100

    add Song class with class methods

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song
+attr_accessor :name, :artist, :genre
+
+@@count = 0
+@@genres =[]
+@@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, g| total[g] += 1 ;total}
+  end
+
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, a| total[a] += 1 ;total}
+  end
+
+end
